a2e71aca35fb9ae754a591a5ce880332f48856bd
Parfait <jenny.filcheck@gmail.com>
2017-04-13 20:13:46 -0500

    completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -4,5 +4,15 @@
 */
 
 const reverseString = (str) => {
-
+  return str.split('').reverse().join('');
 };
+
+/*
+const reverseString = (str) => {
+  let newStr = '';
+  for (let i = str.length-1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+};
+*/
