cacfcde65ffa134c1fd324a154c7720159893a3b
Parfait <jenny.filcheck@gmail.com>
2017-04-22 11:06:16 -0500

    completed

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -3,16 +3,4 @@
   'Hello World' -> 'dlroW olleH'
 */
 
-const reverseString = (str) => {
-  return str.split('').reverse().join('');
-};
-
-/*
-const reverseString = (str) => {
-  let newStr = '';
-  for (let i = str.length-1; i >= 0; i--) {
-    newStr += str[i];
-  }
-  return newStr;
-};
-*/
+const reverseString = str => str.split('').reverse().join('');
