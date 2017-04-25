0ad558b4b65b4f92ada53ab738be196087b625cb
Parfait <jenny.filcheck@gmail.com>
2017-04-24 20:04:43 -0500

    finished longestString

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -3,3 +3,16 @@
  * Return the longest string in the array.
  */
 
+const arr = ['cat', 'dog', 'mouse', 'fish', 'octopus', 'pomeranian'];
+
+const longestString = (arr) => {
+  let max = arr[0];
+
+  for (let i = 0, len = arr.length; i < len; i++) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+};
+
+console.log(longestString(arr));
+
