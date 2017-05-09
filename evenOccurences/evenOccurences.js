5a5c211816bda1b786043b9472f23edcb22f4c0e
Parfait <jenny.filcheck@gmail.com>
2017-05-08 20:09:58 -0500

    finished evenOccurences

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -10,6 +10,25 @@
  *    * console.log(onlyEven); //  4
  *    */
 
+const test = [1, 7, 2, 4, 5, 6, 8, 9, 6, 4];
+const test2 = [1];
+
 const evenOccurence = (arr) => {
-  // Your code here.
+  let firstEvenNum = null;
+
+  for (let i = 0, len = arr.length; i < len; i++) {
+    let count = 0;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === arr[i]) count++;
+    }
+
+    if (count % 2 === 0) {
+      firstEvenNum = arr[i];
+      break;
+    }
+  }
+  return firstEvenNum;
 };
+
+console.log(evenOccurence(test));
