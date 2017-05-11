bd6919ab8e3c1b2225c380ca17493ed948da986c
Parfait <jenny.filcheck@gmail.com>
2017-05-10 20:39:53 -0500

    finished breadthFirstSearch

--- breadthFirstSearch/breadthFirstSearch.js
+++ breadthFirstSearch/breadthFirstSearch.js
@@ -14,3 +14,32 @@
  * };
  * breadthFirstSearch(tree, 2);// will return true before it recursively searches `z`
  */
+const tree = { x: 1, y: 1, z: { x: 1, y: 1, z: 1 }, a: 2 };
+const tree2 = {x: {x: 1, y: 1, z: {x: 1, y: {a: 2}}}, y: {x: 1, y: 1, z: {}}, z: {x: 1}}
+
+const breadthFirstSearch = (tree, value) => {
+  let found = false;
+  
+  const searchBranch = (branch) => {
+    const leaves = Object.keys(branch);
+    const totalLeaves = leaves.length;
+    
+    for (let i = 0; i < totalLeaves; i++) {
+      if (branch[leaves[i]] === value) {
+        found = true;
+        return;
+      }
+    }
+    
+    for (let i = 0; i < totalLeaves; i++) {
+      if (typeof branch[leaves[i]] === 'object') {
+        return searchBranch(branch[leaves[i]]);
+      }
+    }
+  };
+  
+  searchBranch(tree);
+  return found;
+}
+
+console.log(breadthFirstSearch(tree2, 2));
