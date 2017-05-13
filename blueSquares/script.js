a10382138ed0f755296d65bfccb5b7055dca9b6a
Parfait <jenny.filcheck@gmail.com>
2017-05-13 10:40:15 -0500

    completed jquery exercise

--- blueSquares/script.js
+++ blueSquares/script.js
@@ -1,6 +1,14 @@
-
+/*
 $(document).ready(function() {
   $('div').on('click', function() {
     $(this).css('background', 'blue');
   })
+}); */
+
+/*
+$(document).ready(() => {
+  $('div').on('click', (event) => {
+    $(event.target).css('background-color', 'blue');
+  });
 });
+*/
