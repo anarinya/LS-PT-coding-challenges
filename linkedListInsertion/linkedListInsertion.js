482de386891bd2894f41b5d7bc355fb40030be62
Parfait <jenny.filcheck@gmail.com>
2017-05-22 20:00:15 -0500

    linked list insertion challenge

--- linkedListInsertion/linkedListInsertion.js
+++ linkedListInsertion/linkedListInsertion.js
@@ -6,3 +6,44 @@
  * [1] -> [2] -> [2] -> [3]
  * Bonus: Write automated tests
  */
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  insert(val) {
+    let current;
+    const node = {
+      value: val,
+      next: null
+    };
+
+    if (!this.head) {
+      this.head = node;
+    }
+    else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+}
+
+let list = new LinkedList();
+list.insert(1);
+list.insert(2);
+list.insert(3);
+list.insert(2);
+
+
+console.log(list.print());
