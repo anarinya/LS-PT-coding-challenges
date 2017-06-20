7ee49da95e3871ea81bfe0db3a616d7fbb75d942
Parfait <jenny.filcheck@gmail.com>
2017-06-19 20:33:24 -0500

    string compression

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,26 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+const compressString = (str) => {
+  let count = 1;
+  let prev = str[0];
+  let compressed = [];
+
+  for (let i = 1; i <= str.length; i++) {
+    // if the character is the same as the last, increment the counter
+    if (str[i] === prev) count++;
+    // if the character isn't the same as the last...
+    else {
+      // push previous value and count to compressed string
+      compressed.push(`${prev}${count}`);
+      // reset count
+      count = 1;
+    }
+    prev = str[i];
+  }
+  return compressed.join('');
+};
+
+console.log(compressString('aabcccccaaa'));
+console.log(compressString('ddccbbaaqq'));
+console.log(compressString('AaaAaaBbbBbbCccDddeEFFFe'));
