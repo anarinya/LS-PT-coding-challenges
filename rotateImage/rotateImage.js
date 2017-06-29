33a2946eb9ce425c12b97b1c3803bd7dd798cb5a
Parfait <jenny.filcheck@gmail.com>
2017-06-28 20:08:14 -0500

    rotate image

--- rotateImage/rotateImage.js
+++ rotateImage/rotateImage.js
@@ -1,2 +1,20 @@
 // Given an image represented by an NxN matrix, where each pixel in the image is an integer from 0 - 9,
 // write a method to rotate the image by 90 degrees. Can you do this in place?
+
+/* end rotation:
+const matrix = [
+  [3, 2, 1],
+  [3, 2, 1],
+  [3, 2, 1]
+];
+*/
+
+const matrix = [
+  [1, 1, 1],
+  [2, 2, 2],
+  [3, 3, 3]
+];
+
+const rotate = (matrix) => {
+  
+};
